815eaa29f639ca68991af552a108827f2b373d81
Giel <gielvanhoudt@hotmail.com>
2017-09-26 21:48:08 +0200

    added wit recognizer to bot

--- server/controllers/chatbot_controller.js
+++ server/controllers/chatbot_controller.js
@@ -14,7 +14,37 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 
 const WitRecognizer = require('botbuilder-wit');
-const recognizer = new WitRecognizer(process.env.WIT_ACCESS_TOKEN);
+bot.recognizer(new WitRecognizer(process.env.WIT_ACCESS_TOKEN));
 
+bot.dialog('/verkeer', [
+  function(session, args, next) {
+    // Resolve and store any entities passed from LUIS.
+    var intent = args.intent;
+    var location = builder.EntityRecognizer.findEntity(intent.entities, 'wit/location');
+    var destination = builder.dialogData.destination = {
+      location: location ? location.entity : null
+    };
+
+    // Prompt for title
+    if (!alarm.location) {
+        builder.Prompts.text(session, 'Naar waar wil je?');
+    } else {
+        next();
+    }
+  },
+  function(session, results) {
+    var destination = builder.dialogData.destination;
+    if(results.response) {
+      destination.location = results.location;
+    }
+
+    session.endDialog('Het verkeer naar %s wordt opgezocht.', destination.location);
+  }
+]).triggerAction({
+  matches: 'intent.verkeer',
+  confirmPrompt: 'Dit is een confirmprompt'
+})
+
+bot.dialog('/')
 
 exports.process_new_message = connector.listen();
